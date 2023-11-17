f3126fa57827e415112ecc5058c431adaa4fda6c
antonov737 <antonov737x@gmail.com>
2023-11-17 13:43:42 +0000

    changes

--- gordonramsay/server.js
+++ gordonramsay/server.js
@@ -2,13 +2,23 @@ const express = require('express');
 const app = express();
 
 let facts = [
-    "He played for St. Kilda",
-    "Was born in 1929"
-]
+    {"text": "He played for St. Kilda", "tags": ["geography", "history"]},
+    {"text": "Was born in 1929", "tags": ["history"]},
+    {"text": "Is not famous for cookery", "tags": ["friday"]}
+];
 
 app.get("/fact", function(request, response){
     let factNo = parseInt(request.query.n);
-    response.send(facts[factNo]);
+    response.send(facts[factNo].text);
 });
+
+app.get("/tags", function(request, response){
+    let tags = [];
+    for(let fact of facts){
+        tags = tags.concat(fact.tags)
+    }
+    let tagSet = new Set(tags);
+    response.send([...tagSet])
+})
 
 app.listen(8090);
