e6c9e32ed8cdc0f912d91d51faf44c8646d89b35
dcs0spb <s.p.bradley@durham.ac.uk>
2023-11-17 13:43:49 +0000

    Search for tags

--- gordonramsay/server.js
+++ gordonramsay/server.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 
 let facts = [
-    "Played for St Kilda",
-    "Was born in 1929"
+    {"text": "Played for St Kilda", "tags": ["geography", "history"]},
+    {"text": "Was born in 1929", "tags": ["history"]},
+    {"text": "Is not famous for cookery", "tags": ["friday"]}
 ];
 
 app.get("/fact", function (request, response){
     let factNumber = parseInt(request.query.n);
-    response.send(facts[factNumber]);
+    response.send(facts[factNumber].text);
+})
+
+app.get("/tags", function (request, response){
+    let tags = [];
+    for(let fact of facts){
+        tags = tags.concat(fact.tags)
+    }
+    let tagSet = new Set(tags);
+    response.send([...tagSet]);
 })
 
 app.listen(8080);
