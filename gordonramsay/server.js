f15135b5d46e466b8e97dcfd81e95a2b923d5557
dcs0spb <s.p.bradley@durham.ac.uk>
2023-11-17 13:20:52 +0000

    Faultless searching for facts

--- gordonramsay/server.js
+++ gordonramsay/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (request, response){
-    response.send("Gordon is not our hero");
+let facts = [
+    "Played for St Kilda",
+    "Was born in 1929"
+];
+
+app.get("/fact", function (request, response){
+    let factNumber = parseInt(request.query.n);
+    response.send(facts[factNumber]);
 })
 
 app.listen(8080);
