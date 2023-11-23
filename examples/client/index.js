a474d6a694220b4e673caf2c4b1a365962c94f52
dcs0spb <s.p.bradley@durham.ac.uk>
2023-11-23 16:52:20 +0000

    Exceptions for errors

--- examples/client/index.js
+++ examples/client/index.js
@@ -1,6 +1,9 @@
-window.addEventListener('click', function(event){
-    fetch('http://127.0.0.1:8090/list')
-     .then(response => response.text())
-     .then(body =>
-        document.getElementById('content').innerHTML=body)
+window.addEventListener('click', async function(event){
+    try{
+      let response = await fetch('http://127.0.0.1:8090/list');
+      let body = await response.text();
+      document.getElementById('content').innerHTML=body;
+    } catch(e) {
+      alert(e);
+    }
   });
