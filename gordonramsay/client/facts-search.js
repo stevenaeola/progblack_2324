b8fa7fadc8fc00e349a1f921ef7408d57b9d42af
dcs0spb <s.p.bradley@durham.ac.uk>
2023-11-24 13:43:32 +0000

    fetch working with raw JSON from form

--- gordonramsay/client/facts-search.js
+++ gordonramsay/client/facts-search.js
@@ -1,1 +1,17 @@
-alert("loaded fecthj script");
+let ff = document.getElementById("facts-form")
+
+ff.addEventListener('submit', async function(event){
+    event.preventDefault();
+    try{
+      let tag = document.getElementById("facts-tag").value;
+      let response = await fetch('http://127.0.0.1:8080/facts?tag=' + tag);
+      if(response.ok){
+      let body = await response.text();
+      document.getElementById('facts-container').innerHTML=body;
+    } else{
+        alert("Sorry you cannot type you have a 404");
+    }
+    } catch(e) {
+      alert(e);
+    }
+  });
