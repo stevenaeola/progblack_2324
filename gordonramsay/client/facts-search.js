5458971fd06dda42473669bdcc46ef90154fb036
dcs0spb <s.p.bradley@durham.ac.uk>
2023-11-24 14:03:07 +0000

    Finished tag search, starting buttons for tag searching

--- gordonramsay/client/facts-search.js
+++ gordonramsay/client/facts-search.js
@@ -1,3 +1,13 @@
+function setupTagButtons(){
+    let tags = await fetch('http://127.0.0.1:8080/tags');
+    if(response.ok){
+    let facts = await response.json();
+    }
+    for(let tag in tags){
+        alert(tag);
+    }
+}
+
 let ff = document.getElementById("facts-form")
 
 ff.addEventListener('submit', async function(event){
@@ -9,7 +19,7 @@ ff.addEventListener('submit', async function(event){
       let facts = await response.json();
       let html = "<ul>\n";
       for(let fact of facts){
-        html += "<li>" + fact + "</li>\n";
+        html += `<li>${fact}</li>\n`;
       }
       html += "</ul>\n";
       document.getElementById('facts-container').innerHTML = html;
