3299bce9035e605b2d65b4d1eb7c1cca6d567e76
dcs0spb <s.p.bradley@durham.ac.uk>
2023-11-24 13:10:36 +0000

    Fixed assets to be served through node

--- gordonramsay/server.js
+++ gordonramsay/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.static('client'));
 
 let facts = [
     {"text": "Played for St Kilda", "tags": ["geography", "history"]},
