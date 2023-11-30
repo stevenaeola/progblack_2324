beb921a26aac40e1b28ece58b8209216858bd293
dcs0spb <s.p.bradley@durham.ac.uk>
2023-11-30 15:52:26 +0000

    Buttons from tags closes #66

--- gordonramsay/client/facts-search.js
+++ gordonramsay/client/facts-search.js
@@ -1,11 +1,16 @@
-function setupTagButtons(){
-    let tags = await fetch('http://127.0.0.1:8080/tags');
-    if(response.ok){
+async function loadTaggedFacts(tag){
+  let response = await fetch('http://127.0.0.1:8080/facts?tag=' + tag);
+  if(response.ok){
     let facts = await response.json();
+    let html = "<ul>\n";
+    for(let fact of facts){
+      html += `<li>${fact}</li>\n`;
     }
-    for(let tag in tags){
-        alert(tag);
-    }
+    html += "</ul>\n";
+    document.getElementById('facts-container').innerHTML = html;
+  } else{
+      alert("Sorry you cannot type you have a 404");
+  }
 }
 
 let ff = document.getElementById("facts-form")
@@ -14,19 +19,21 @@ ff.addEventListener('submit', async function(event){
     event.preventDefault();
     try{
       let tag = document.getElementById("facts-tag").value;
-      let response = await fetch('http://127.0.0.1:8080/facts?tag=' + tag);
-      if(response.ok){
-      let facts = await response.json();
-      let html = "<ul>\n";
-      for(let fact of facts){
-        html += `<li>${fact}</li>\n`;
-      }
-      html += "</ul>\n";
-      document.getElementById('facts-container').innerHTML = html;
-    } else{
-        alert("Sorry you cannot type you have a 404");
-    }
+      loadTaggedFacts(tag);
     } catch(e) {
       alert(e);
     }
   });
+
+  async function setupTagButtons(){
+    let response = await fetch('http://127.0.0.1:8080/tags');
+    if(response.ok){
+      let tags = await response.json();
+      let container = document.getElementById("tag-button-container");
+      for(let tag of tags){
+        container.innerHTML += `<div class="col"><button class="btn btn-secondary tag-button">${tag}</button></div>`;
+      }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", setupTagButtons);
